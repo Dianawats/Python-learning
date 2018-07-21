ef2926b6d20c1d9fc8a9940a6e2355801d9eecf0
Dianawats <dianakiznaki@gmail.com>
2018-07-21 06:42:39 -0700

    adding statements file

---
+++ statements.py
@@ -0,0 +1,17 @@
+#if, elif, and else statements
+x = False:
+if x:
+    print 'x was True'
+else:
+    print 'I will print if x is anything not True'
+    #results, I will print if x is anything not True
+
+x = True:
+if x:
+    print 'x was True'
+else:
+    print 'I will print if x is anything not True'
+#results x was True
+
+
+#nested structure
