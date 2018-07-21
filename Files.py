c33d323d50039b87ebd69dbcfe6bed224ce57490
Dianawats <dianakiznaki@gmail.com>
2018-07-21 02:57:01 -0700

    adding the file objects

---
+++ Files.py
@@ -0,0 +1,4 @@
+#File objects, Help to interact with external files on computer
+#Note when using this, you have to install some libraries or modules to interact with those various file types
+#Built-in open function that allows us to open & play with basic file types
+
