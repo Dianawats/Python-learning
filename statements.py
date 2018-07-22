0d84b14deceaa3af7fdbce598770a5a00987e317
Dianawats <dianakiznaki@gmail.com>
2018-07-22 11:31:11 -0700

    modifying statements file

--- statements.py
+++ statements.py
@@ -15,3 +15,47 @@ else:
 
 
 #nested structure
+loc == 'Mall'
+if loc == 'Auto shop':
+    print 'Welcome to the Auto Shop!'
+elif loc == 'Bank':
+    print 'Welcome to the Bank!'
+else:
+    print 'where are you'
+#prints where are you? results
+
+loc == 'Auto shop'
+if loc == 'Auto shop':
+    print 'Welcome to the Auto Shop!'
+elif loc == 'Bank':
+    print 'Welcome to the Bank!'
+else:
+    print 'where are you'
+#results welcome to the autoshop
+
+loc == 'Mall'
+if loc == 'Auto shop':
+    print 'Welcome to the Auto Shop!'
+elif loc == 'Bank':
+    print 'Welcome to the Bank!'
+elif loc == 'Mall':
+    print 'welcome to the mall!'
+else:
+    print 'where are you'
+#results, welcome to the mall!
+
+person == 'sam'
+if person == 'sam':
+    print 'Hi sam'
+else:
+    print "what's your name?"
+#results, Hi Sam
+
+person == 'sam'
+#results, True #boolean
+
+
+
+
+
+
