8234994834abff04e65db2d2c9e90f6c3442e8de
Dianawats <dianakiznaki@gmail.com>
2018-07-23 22:53:37 +0300

    Adding function file

---
+++ functions.py
@@ -0,0 +1,17 @@
+# A function is a useful device that groups together a set of statements so they can be run more than once
+#can also let us specify parameters that can serve as inputs to the function
+# ALso allows us not to have repeatedly write the same code again and again
+#Buiding blocks when constructing larger and amounts of codes to solve problems
+#def inbuilt key word
+def name_of_function():
+    pass
+
+#another example
+def my_addition_func(arg1,agr2):
+    print arg1 + agr2
+my_addition_func(1,2)
+3 #results
+
+#docstring
+
+
