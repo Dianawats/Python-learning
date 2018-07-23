86eadf1a5f455681b9791612499103839f44e67c
Dianawats <dianakiznaki@gmail.com>
2018-07-23 22:18:31 +0300

    Adding methods file

---
+++ Methods.py
@@ -0,0 +1,14 @@
+#methods are essentially functions built in objects
+#Methods perform specific actions on the objects also take arguments just like functions
+#Methods are in for of:
+object.method(arg1, arg2,etc) #syntax
+
+#example of methods in a list
+l = [1,2,3,4,5]
+l.append(6)
+l #results
+[1,2,3,4,5,6]
+
+
+
+
