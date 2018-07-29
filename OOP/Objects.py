08e685cfe7b3ef7c610a233330808ee4cb1008c8
Dianawats <dianakiznaki@gmail.com>
2018-07-29 14:40:30 +0300

    Creating OOP folder and object file

---
+++ OOP/Objects.py
@@ -0,0 +1,14 @@
+#in python everything is an object
+l = [1,2,3]
+l.count(2)
+1 #results, it has appeared once in a list
+
+def square(num):
+    return num**2
+
+type(square)
+function #results. It's a function object and everything in python is an object
+
+
+
+
